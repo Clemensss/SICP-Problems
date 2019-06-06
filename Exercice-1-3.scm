01164a0140294e923899ffe6ba3ff9afa58b8799
Clemensss <schrageclemens@gmail.com>
2019-06-06 14:27:24 -0300

    first

---
+++ Exercice-1-3.scm
@@ -0,0 +1,8 @@
+(define (largesquare x y z)
+    (cond((< x y z)(sum-squares y z)) 
+	 ((< y x z)(sum-squares x z))
+	 ((< z x y)(sum-squares x y))))
+(define (square x)(* x x))
+(define (sum-squares x y)
+    (+ (square x) (square y)))
+(print(largesquare 2 3 4))
