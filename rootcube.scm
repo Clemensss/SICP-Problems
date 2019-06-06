8f5fda69933e0dc89070aa88bae3c8d70d3d97e5
Clemensss <schrageclemens@gmail.com>
2019-06-06 18:15:07 -0300

    ate q deu mas eh foda entender essa merda as vezes

---
+++ rootcube.scm
@@ -0,0 +1,18 @@
+(define(cube x)(* x x x))
+(define(square x)(* x x))
+(define(absolute x)
+    (if(< x 0)(- x)x))
+(define(root x)
+    (cube-iter x 1.0))
+(define(cube-iter x guess)
+    (if(good? x guess) 
+	guess
+    (cube-iter x (improved x guess))))
+(define(good? x guess)
+    (<(absolute(- (cube guess) x)) 0.001))
+(define(improved x guess)
+    (newton x guess))
+(define(newton x guess)
+    (/ (+ (/ x (square guess)) (* guess 2)) 3))
+(print(root 125)) 
+    
